a71dc82025b31a953fa8d867ace8bd10501b007f
Luciano Silveira <ladrians@gmail.com>
2018-11-13 01:44:41 -0800

    minimal code reorganized

--- pr2_robot/scripts/project_template.py
+++ pr2_robot/scripts/project_template.py
@@ -54,7 +54,7 @@ def pcl_callback(ros_pcl_msg):
     # Convert ROS msg to PCL data
     pcl_msg = ros_to_pcl(ros_pcl_msg)
     
-    # TODO: Statistical Outlier Filtering
+    # Statistical Outlier Filtering
     outlier_filter = pcl_msg.make_statistical_outlier_filter()
     outlier_filter.set_mean_k(50)
     x = 1.0
@@ -84,8 +84,8 @@ def pcl_callback(ros_pcl_msg):
 
     # Extract inliers and outliers
     inliers, coefficients = seg.segment()
-    extracted_inliers = cloud_filtered.extract(inliers, negative=False) # Table detail
-    extracted_outliers = cloud_filtered.extract(inliers, negative=True) # Objects detail
+    extracted_inliers = cloud_filtered.extract(inliers, negative=False) # Table
+    extracted_outliers = cloud_filtered.extract(inliers, negative=True) # Objects
 
     # Euclidean Clustering
     white_cloud = XYZRGB_to_XYZ(extracted_outliers)
@@ -161,10 +161,6 @@ def pcl_callback(ros_pcl_msg):
     # Publish the list of detected objects
     detected_objects_pub.publish(detected_objects)
 
-    # TODO LS check this
-    # Suggested location for where to invoke your pr2_mover() function within pcl_callback()
-    # Could add some logic to determine whether or not your object detections are robust
-    # before calling pr2_mover()
     try:
         pr2_mover(detected_objects)
     except rospy.ROSInterruptException:
@@ -174,6 +170,10 @@ def pcl_callback(ros_pcl_msg):
 def pr2_mover(object_list):
 
     # TODO: Initialize variables
+    yaml_filename = 'output_1.yaml'
+    test_scene_num = Int32()
+    centroids = []
+    dict_list = []
 
     # TODO: Get/Read parameters
 
@@ -182,14 +182,32 @@ def pr2_mover(object_list):
     # TODO: Rotate PR2 in place to capture side tables for the collision map
 
     # TODO: Loop through the pick list
+    for index, obj in enumerate(object_list):
 
-        # TODO: Get the PointCloud for a given object and obtain it's centroid
+        # Get the PointCloud for a given object and obtain it's centroid
+        points_arr = ros_to_pcl(obj.cloud).to_array()
+        centroids.append(np.mean(points_arr, axis=0)[:3])
+        center_x = np.asscalar(centroids[index][0])
+        center_y = np.asscalar(centroids[index][1])
+        center_z = np.asscalar(centroids[index][2])
+
+        object_name = String()
+        object_name.data = obj['name']
+        arm_name = String()
+        arm_name.data = '' # TODO
 
         # TODO: Create 'place_pose' for the object
+        place_pose = Pose()
 
         # TODO: Assign the arm to be used for pick_place
+        pick_pose = Pose()
+        pick_pose.position.x = center_x
+        pick_pose.position.y = center_y
+        pick_pose.position.z = center_z
 
         # TODO: Create a list of dictionaries (made with make_yaml_dict()) for later output to yaml format
+        yaml_dict = make_yaml_dict(test_scene_num, arm_name, object_name, pick_pose, place_pose)
+        dict_list.append(yaml_dict)
 
         # Wait for 'pick_place_routine' service to come up
         rospy.wait_for_service('pick_place_routine')
@@ -198,7 +216,7 @@ def pr2_mover(object_list):
             pick_place_routine = rospy.ServiceProxy('pick_place_routine', PickPlace)
 
             # TODO: Insert your message variables to be sent as a service request
-            resp = pick_place_routine(TEST_SCENE_NUM, OBJECT_NAME, WHICH_ARM, PICK_POSE, PLACE_POSE)
+            resp = pick_place_routine(test_scene_num, object_name, arm_name, pick_pose, place_pose)
 
             print ("Response: ",resp.success)
 
@@ -206,8 +224,7 @@ def pr2_mover(object_list):
             print "Service call failed: %s"%e
 
     # TODO: Output your request parameters into output yaml file
-
-
+    send_to_yaml(yaml_filename, dict_list)
 
 if __name__ == '__main__':
 
@@ -215,10 +232,10 @@ if __name__ == '__main__':
     rospy.init_node('object_recognition', anonymous=True)
 
     # Create Subscribers
-    pcl_sub = rospy.Subscriber("/sensor_stick/point_cloud", pc2.PointCloud2, pcl_callback, queue_size=1)
+    pcl_sub = rospy.Subscriber("/pr2/world/points", pc2.PointCloud2, pcl_callback, queue_size=1)
     # Create Publishers
-    #pcl_objects_pub = rospy.Publisher("/pcl_objects", PointCloud2, queue_size=1)
-    #pcl_table_pub = rospy.Publisher("/pcl_table", PointCloud2, queue_size=1)
+    pcl_objects_pub = rospy.Publisher("/pcl_objects", PointCloud2, queue_size=1)
+    pcl_table_pub = rospy.Publisher("/pcl_table", PointCloud2, queue_size=1)
     object_markers_pub = rospy.Publisher("/object_markers", Marker, queue_size=1)
     detected_objects_pub = rospy.Publisher("/detected_objects", DetectedObjectsArray, queue_size=1)
 
